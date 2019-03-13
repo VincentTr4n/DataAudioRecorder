50977cde380b7d59df92d3161f71808b69f9d652
VincentTr4n <33866720+VincentTr4n@users.noreply.github.com>
2019-03-13 10:51:24 +0700

    first commit

---
+++ app/src/main/java/com/example/cotye/dataaudiorecorder/Util.java
@@ -0,0 +1,28 @@
+package com.example.cotye.dataaudiorecorder;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.io.File;
+import java.util.Random;
+
+public class Util {
+    public static void requestPermission(Activity activity, String permission) {
+        if (ContextCompat.checkSelfPermission(activity, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, 0);
+        }
+    }
+    public static String createFilePath(String path, String commander){
+        Random rnd = new Random();
+        int num = rnd.nextInt();
+        File file = new File(path + "/" + commander +"_" + String.valueOf(num) + ".wav");
+        while(file.exists()){
+            num = rnd.nextInt();
+            file = new File(path + "/" + commander +"_" + String.valueOf(num) + ".wav");
+        }
+        return file.toString();
+    }
+}
